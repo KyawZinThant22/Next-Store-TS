e7ea196cd7af802ce7de2e7c2f709f0457266a99
KyawZinThant22 <kyawthantver2@gmail.com>
2023-04-28 22:46:02 +0530

    test ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-   return <div className="bg-red-400 h-screen w-full">hello ci/cd</div>;
+   return <div className="bg-red-400 h-screen w-full">hello my love nay chi hsu pyae</div>;
 }
 
 export default App;
