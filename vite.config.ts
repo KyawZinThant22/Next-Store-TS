aee83ebdf19f57140279d41f6d991337215c7296
KyawZinThant22 <kyawthantver2@gmail.com>
2023-04-28 22:20:49 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+   build: {
+      target: 'es2020', // Target ECMAScript 2020 (ES11)
+      outDir: 'build', // Output directory for production build
+   },
+   plugins: [react(), tsconfigPaths()],
 });
