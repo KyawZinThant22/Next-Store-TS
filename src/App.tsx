dc4c4b6d06247d1264ba39e5d671cb425f7526d9
KyawZinThant22 <kyawthantver2@gmail.com>
2023-05-01 23:25:38 +0530

    add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import Routes from './routes/index';
 function App() {
    return (
-      <div>
-         <Routes />
-      </div>
+      <AnimatePresence>
+         <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 2, ease: 'easeIn' }}
+         >
+            <Routes />
+         </motion.div>
+      </AnimatePresence>
    );
 }
 
