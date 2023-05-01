add2de6f66d99e82fd4d509a30e55e4992d9f31d
KyawZinThant22 <kyawthantver2@gmail.com>
2023-05-01 23:22:22 +0530

    create animated button with framer motion

--- src/components/Elements/Button.tsx
+++ src/components/Elements/Button.tsx
@@ -1,3 +1,5 @@
+import { motion, AnimatePresence } from 'framer-motion';
+
 interface IButtonProps {
    label: string;
    action?: () => void | any;
@@ -7,19 +9,37 @@ interface IButtonProps {
    arialabel: string;
 }
 
+const buttonVariants = {
+   hover: {
+      backgroundColor: '#ffffff',
+      color: '#013D28',
+      transition: {
+         duration: 0.3,
+         yoyo: Infinity,
+      },
+   },
+};
+
 const Button = ({ label, action, variant, type, arialabel }: IButtonProps) => {
    return (
-      <button
-         onClick={action}
-         aria-label={arialabel}
-         type={type}
-         className={`text-md rounded-full p-2 px-5 font-medium text-white md:p-3 md:px-6 md:text-lg ${
-            variant == 'contained' &&
-            'border-2 border-skin-jade bg-skin-jade font-bold hover:bg-white hover:text-skin-jade'
-         } `}
-      >
-         {label}
-      </button>
+      <AnimatePresence>
+         <motion.button
+            variants={buttonVariants}
+            whileHover="hover"
+            exit={{
+               background: '#013D28',
+               color: '#fff',
+            }}
+            onClick={action}
+            aria-label={arialabel}
+            type={type}
+            className={`text-md rounded-full p-2 px-5 font-medium text-white md:p-3 md:px-6 md:text-lg ${
+               variant == 'contained' && 'border-2 border-skin-jade bg-skin-jade   font-bold '
+            } `}
+         >
+            {label}
+         </motion.button>
+      </AnimatePresence>
    );
 };
 
